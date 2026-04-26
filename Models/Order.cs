8ca4f31179c9100c856f2c38e5797d92ea9ab3e7
TheCapercaillie <jonathan.tjader@gmail.com>
2026-04-26 19:24:25 +0200

    Updated the admin page a bit more for example added AdminOrders.razor

--- Models/Order.cs
+++ Models/Order.cs
@@ -9,5 +9,10 @@
         public decimal TotalPrice { get; set; }
 
         public string Status { get; set; } = "Pending"; // Pending, Paid, Failed
+
+        public List<OrderItem> OrderItems { get; set; } = new();
+
+        public string CustomerEmail { get; set; } = string.Empty;
+        public string CustomerName { get; set; } = string.Empty;
     }
 }
