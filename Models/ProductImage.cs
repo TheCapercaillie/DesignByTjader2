0b8e8b20e7f8c4440521553f3c6bab6be55e27ba
TheCapercaillie <jonathan.tjader@gmail.com>
2026-04-27 14:58:23 +0200

    Added imgage deletion in adminproducs and also a way to change the main image thumbnail and i also updated app.css improving the UI and design

--- Models/ProductImage.cs
+++ Models/ProductImage.cs
@@ -5,8 +5,9 @@
         public int Id { get; set; }
 
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product Product { get; set; } = null!;
 
         public string ImageUrl { get; set; } = string.Empty;
+        public int SortOrder { get; set; }
     }
 }
